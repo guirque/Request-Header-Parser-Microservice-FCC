66fb7e99b754ad2777fbb3c451d78d924c1c57a4
Guilherme <guirque@gmail.com>
2023-01-08 18:51:31 -0300

    Adding GET Request for 'whoami'
    Accessing /api/whoami will respond with the user's ip address, and the corresponding language and software values of the request headers.

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// whoami
+app.get('/api/whoami', function(req, res)
+{
+  const response = 
+  {
+    "ipaddress": req.ip,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent'],
+  };
+  res.json(response);
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
